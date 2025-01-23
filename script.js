127d5a21cfd5d07c28e8322f71a308a7bd35d21a
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-23 15:47:14 +0800

    Add logic to get human choice
    - Implemented `getHumanChoice` function to capture user input using `prompt`.
    - Ensured input validation to handle incorrect choices by prompting the user again.
    - Converted the input to lowercase and trimmed whitespace for consistent comparison.

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const getComputerChoice = function () {
   const options = ['rock', 'paper', 'scissors'];
   return options[randomNumber];
 };
+
+const getHumanChoice = function () {
+  const choice = prompt('Choose your weapon: Rock/Paper/Scissors')
+    .toLowerCase()
+    .trim();
+  return choice === 'rock' || choice === 'paper' || choice === 'scissors'
+    ? choice
+    : prompt('⛔ Invalid choice. Please choose Rock, Paper, or Scissors:')
+        .toLowerCase()
+        .trim();
+};
